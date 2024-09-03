1f9f549adea4ad1727bd833fd8bc4311441ae60f
Kryolyz <danwilmes@gmail.com>
2024-09-03 22:15:23 +0200

    Some cleaning

--- extension.js
+++ extension.js
@@ -1,24 +1,13 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 const vscode = require('vscode');
 const { OpenAI } = require('openai');
-// import OpenAI from 'openai';
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, "gippity" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('gippity.helloWorld', function () {
 		vscode.window.showInformationMessage('Hello World from gippity!');
 	});
@@ -41,6 +30,9 @@ function activate(context) {
 		}
 
 		const apiKey = vscode.workspace.getConfiguration().get('gippity.apiKey');
+		if (!apiKey) {
+			console.error("No API key set in settings. Press 'ctrl+,' (control and comma) to open settings and search for 'gippity.apiKey', then insert yours.")
+		}
 		const openai = new OpenAI({ apiKey: apiKey });
 
 		// Get the entire file content for context
